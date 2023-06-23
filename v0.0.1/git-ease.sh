462f19e757f0c47aa7f56b92698fa6849c9b5290
kekeli-the-light <kekelidompeh@gmail.com>
2023-06-23 13:37:47 +0000

    git-ease.sh

--- v0.0.1/git-ease.sh
+++ v0.0.1/git-ease.sh
@@ -2,7 +2,6 @@
 
 file=$2
 mess=$3
-
 afca(){
 	git add $file
 	git commit -m "$mess"
@@ -13,11 +12,23 @@ aca(){
 	git commit -m "$mess"
 }
 
+push(){
+	arg1=$1
+	git push -u origin $1
+}
+
+
 if [ "$1" == "-f" ];
 then
 	afca
 elif [ "$1" == "-a" ];
 then
 	aca
+elif ["$1" == "-f" && $2 == "-p"];
+then
+	afca
+	echo Branch name:
+	read branch
+	push branch
 fi
 
