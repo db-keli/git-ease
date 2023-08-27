6e2a1780e4e41c58bcfc753543ac805d685c6fcd
kekeli-the-light <kekelidompeh@gmail.com>
2023-08-27 16:19:54 +0000

    doing this

--- Python/Squasher/squash.py
+++ Python/Squasher/squash.py
@@ -7,17 +7,19 @@ import sys
 def add_file_commit(args):
     if any(arg == '-f' for arg in args):
         index = args.index('-f')
-        if index + 1 < len(args):
+        if index + 2 < len(args):
             file_to_add = args[index+1]
+            commit_message = args[index+2]
             command = ['git', 'add', file_to_add]
-
+            commit = ['git', 'commit', '-m', commit_message]
             try:
                 subprocess.run(command, check=True)
                 print(f"Successfully added {file_to_add}")
+                subprocess.run(commit, check=True)
+                print(f"Commited {file_to_add}")
+
             except subprocess.CalledProcessError as error:
                 print(f"{error}")
-        else:
-            print("No file specified after -f argument")
     else:
         print("No argument found")
 
