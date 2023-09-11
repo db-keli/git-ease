cc0e108507db9eeb22666b674d0551f2318c0015
kekeli-the-light <kekelidompeh@gmail.com>
2023-09-11 08:21:30 +0000

    add this

--- python/main.py
+++ python/main.py
@@ -9,10 +9,11 @@ import subprocess
 args = sys.argv
 
 #Add a file and commit
-for arg in args:
-    match_AddAndCommit1 = re.findall('-', arg)
-    match_AddAndCommit2 = re.findall('', arg)
-    print(match_AddAndCommit1)
+# for arg in args:
+#     arg = arg.rstrip()
+#     match_ = re.findall('-', arg)
+#     match_f = re.findall('', arg)
+#     print(match_AddAndCommit1)
 
 # if match_AddAndCommit1 != None:
 #     print('This is when I do this shit')
@@ -22,8 +23,29 @@ for arg in args:
 # if match_AddAndCommit2 !=None:
 #     print 
 
-if '-f' in args and '-p' in args:
-    print('W')
+# if '-f' in args and '-p' in args:
+#     print('W')
 
-elif '-fp' in args or '-pf' in args:
-    print('L')
+# elif '-fp' in args or '-pf' in args:
+#     print('L')
+
+if args[1] == '-f' and args[2] != '-p':
+    index = args.index('-f')
+    addFileAndCommit(args, index)
+
+elif args[1] == 'a' and args[2] != '-p':
+    index = args.index('-a')
+    addAllAndCommit(args, index)
+
+elif len(args) >= 5:
+    for arg in args:
+        match1 = re.findall('-fp', arg)
+        match2 = re.findall('pf', arg)
+    
+    if match1 != None or match2 != None:
+        index1 = args.index('pf')
+        # index2 = args.index('fp')
+        if index1:
+            addFileAndCommit(args, index1)
+            push
+        
