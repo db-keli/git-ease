5a180709cdd40e3092fbb8095bae50c65e525548
kekeli-the-light <kekelidompeh@gmail.com>
2023-09-11 08:32:45 +0000

    main.py

--- python/main.py
+++ python/main.py
@@ -8,27 +8,6 @@ import subprocess
 
 args = sys.argv
 
-#Add a file and commit
-# for arg in args:
-#     arg = arg.rstrip()
-#     match_ = re.findall('-', arg)
-#     match_f = re.findall('', arg)
-#     print(match_AddAndCommit1)
-
-# if match_AddAndCommit1 != None:
-#     print('This is when I do this shit')
-#     print(args[1])
-
-# #When arg is 
-# if match_AddAndCommit2 !=None:
-#     print 
-
-# if '-f' in args and '-p' in args:
-#     print('W')
-
-# elif '-fp' in args or '-pf' in args:
-#     print('L')
-
 if args[1] == '-f' and args[2] != '-p':
     index = args.index('-f')
     addFileAndCommit(args, index)
@@ -48,3 +27,4 @@ elif (re.search('-fp', arg) for arg in args) or (re.search('pf', arg) for arg in
         # index2 = args.index('fp')
         if index1:
             addFileAndCommit(args, index1)
+            push()
