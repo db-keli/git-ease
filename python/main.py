54e54430337935909a09057d9d09ae975f7eb024
kekeli-the-light <kekelidompeh@gmail.com>
2023-09-11 08:31:27 +0000

    main.py

--- python/main.py
+++ python/main.py
@@ -33,19 +33,18 @@ if args[1] == '-f' and args[2] != '-p':
     index = args.index('-f')
     addFileAndCommit(args, index)
 
-elif args[1] == 'a' and args[2] != '-p':
+elif args[1] == '-a' and args[2] != '-p':
     index = args.index('-a')
     addAllAndCommit(args, index)
 
-elif len(args) >= 5:
+elif (re.search('-fp', arg) for arg in args) or (re.search('pf', arg) for arg in args):
     for arg in args:
         match1 = re.findall('-fp', arg)
-        match2 = re.findall('pf', arg)
+        match2 = re.findall('-pf', arg)
+        print(f"This is {match1} and this is {match2}")
     
     if match1 != None or match2 != None:
-        index1 = args.index('pf')
+        index1 = args.index('-pf')
         # index2 = args.index('fp')
         if index1:
             addFileAndCommit(args, index1)
-            push
-        
