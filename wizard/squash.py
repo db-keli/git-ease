d9adfd6904cdafd628c7b9e8bd41ebbac76e70c1
kekeli-the-light <kekelidompeh@gmail.com>
2023-10-04 16:22:20 +0000

    /home/kekeli/Repos/alx-low_level_programming/

--- wizard/squash.py
+++ wizard/squash.py
@@ -1,7 +1,8 @@
 #!usr/bin/env python3
 import subprocess
 
-def addFileAndCommit(args, index):
+
+def add_file_and_commit(args, index):
     if any(arg == '-f' for arg in args):
         if index + 2 < len(args):
             file_to_add = args[index+1]
@@ -14,7 +15,7 @@ def addFileAndCommit(args, index):
                 print(f"Successfully Committed {file_to_add}")
 
             except subprocess.CalledProcessError as error:
-                print(f"{error}")
+                print(f"There's nothing to commit{error}")
             except IndexError:
                 print(f"List index is out of range,")
                 print("Could be because arguments are not placed well") # Suggest to check documentation over here
@@ -31,10 +32,10 @@ def addFileAndCommit(args, index):
                 print(f"Successfully Committed {file_to_add}")
 
             except subprocess.CalledProcessError as error:
-                print(f"{error}")
+                print(f" There's nothing to commit{error}")
             except IndexError:
                 print(f"List index is out of range,")
-                print("Could be because arguments are not placed well") # Suggest to check documentation over here
+                print("Could be because arguments are not placed well")  # Suggest to check documentation over here
         
     else:
         print("No argument found")
@@ -57,35 +58,35 @@ def addFileAndCommit(args, index):
 #         print("No argument found")
 
 
-def addAllAndCommit(args, index):
+def add_all_and_commit(args, index):
     if any(arg == '-a' for arg in args):
-        message = args[index +1]
+        commit_message = args[index + 1]
         command1 = ['git', 'add', '.']
-        command2 = ['git', 'commit',  '-m', f'{message}']
+        command2 = ['git', 'commit',  '-m', f'{commit_message}']
         try:
             subprocess.run(command1, check=True)
             subprocess.run(command2, check=True)
             print(f"Successfully committed all files")
         except subprocess.CalledProcessError as error:
-            print(f"{error}")
+            print(f" There's nothing to commit{error}")
         except IndexError:
             print(f"List index is out of range,")
-            print("Could be because arguments are not placed well") # Suggest to check documentation over here
+            print("Could be because arguments are not placed well")  # Suggest to check documentation over here
             
     elif any(arg == '-ap' for arg in args) or any(arg == '-pa' for arg in args):
         if index:
             commit_message = args[index+1]
             command = ['git', 'add', '.']
-            commit = ['git', 'commit', '-m', commit_message] # try to commit one file
+            commit = ['git', 'commit', '-m', commit_message]  # try to commit one file
             try:
                 subprocess.run(command, check=True)
                 subprocess.run(commit, check=True)
                 print(f"Successfully Committed files")
             except subprocess.CalledProcessError as error:
-                print(f"{error}")
+                print(f"There's nothing to commit{error}")
             except IndexError:
                 print(f"List index is out of range,")
-                print("Could be because arguments are not placed well") # Suggest to check documentation over here
+                print("Could be because arguments are not placed well")  # Suggest to check documentation over here
         
 
 def push():
@@ -95,14 +96,14 @@ def push():
         print(f"Commits have been pushed!")
     except subprocess.CalledProcessError as error:
         print(f"{error}")
-    except IndexError as error:
+    except IndexError:
         print(f"List index is out of range,")
-        print("Could be because arguments are not placed well") # Suggest to check documentation over here
+        print("Could be because arguments are not placed well")  # Suggest to check documentation over here
 
 
-def timeTravel(commit_id):
-    commit_id = str(commit_id)
-    print(commit_id)
+# def timeTravel(commit_id):
+#     commit_id = str(commit_id)
+#     print(commit_id)
 
 
 if __name__:
--- wizard/wand.py
+++ wizard/wand.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from wizard import squash
+from squash import add_all_and_commit, add_file_and_commit, push
 import re
 import sys
 
@@ -12,15 +12,18 @@ def main():
 
     # When there's only -f argument(add and commit only)
 
+    match1 = None
+    match2 = None
+
     if args[1] == '-f' and args[2] != '-p':
         index = args.index('-f')
-        squash.addFileAndCommit(args, index)
+        add_file_and_commit(args, index)
 
     # When arguments begin with -f -p
     elif args[1] == '-f' and args[2] == '-p':
         index = args.index('-p')
-        squash.addFileAndCommit(args, index)
-        squash.push()
+        add_file_and_commit(args, index)
+        push()
 
     # When arguments begin with -fp or -pf(add, commit and push)   
     elif (re.search('-fp', arg) for arg in args) or (re.search('pf', arg) for arg in args):
@@ -30,25 +33,25 @@ def main():
         
         if match1:
             index1 = args.index('-pf')
-            squash.addFileAndCommit(args, index1)
-            squash.push()
+            add_file_and_commit(args, index1)
+            push()
         elif match2:
             index2 = args.index('-fp')
-            squash.addFileAndCommit(args, index2)
-            squash.push()
+            add_file_and_commit(args, index2)
+            push()
 
     # Add all files and commit/commit and push
 
     # When arguments begin with -a only(add and commit only)
     if args[1] == '-a':
         index = args.index('-a')
-        squash.addAllAndCommit(args, index)
+        add_all_and_commit(args, index)
 
     # When arguments begin with -a -p(add, commit and push them)
     elif args[1] == '-a' and args[2] == '-p':
         index = args.index('-p')
-        squash.addAllAndCommit(args, index)
-        squash.push()
+        add_all_and_commit(args, index)
+        push()
 
     # When arguments begin with -ap or -pa(add, commit and push files)
     elif (re.search('-ap', arg) for arg in args) or (re.search('-pa', arg) for arg in args):
@@ -59,8 +62,8 @@ def main():
         if match1 is not None:
             try:
                 index1 = args.index('-ap')
-                squash.addAllAndCommit(args, index1)
-                squash.push()
+                add_all_and_commit(args, index1)
+                push()
                 print("shit worked")
             except ValueError:
                 pass
@@ -68,8 +71,8 @@ def main():
         if match2 is not None:
             try:
                 index2 = args.index('-pa')
-                squash.addAllAndCommit(args, index2)
-                squash.push()
+                add_all_and_commit(args, index2)
+                push()
             except ValueError:
                 pass
 
