646f5d84b6168ba1f918b9c4c288b1fe1ca21037
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2023-11-08 19:26:56 +0000

    bug fix

--- wizard/wand.py
+++ wizard/wand.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from squash import add_all_and_commit, add_file_and_commit, push
+from . import squash
 import re
 import os
 import sys
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 def main():
     args = sys.argv
 
-    # Add one file and commit/commit and push 
+    # Add one file and commit/commit and squash.push 
 
     # When there's only -f argument(add and commit only)
 
@@ -21,15 +21,15 @@ def main():
 
     if args[1] == '-f' and args[2] != '-p':
         index = args.index('-f')
-        add_file_and_commit(args, index)
+        squash.add_file_and_commit(args, index)
 
     # When arguments begin with -f -p
     elif args[1] == '-f' and args[2] == '-p':
         index = args.index('-p')
-        add_file_and_commit(args, index)
-        push()
+        squash.add_file_and_commit(args, index)
+        squash.push()
 
-    # When arguments begin with -fp or -pf(add, commit and push)   
+    # When arguments begin with -fp or -pf(add, commit and squash.push)   
     elif (re.search('-fp', arg) for arg in args) or (re.search('pf', arg) for arg in args):
         for arg in args:
             match1 = re.findall('-fp', arg)
@@ -37,27 +37,27 @@ def main():
         
         if match1:
             index1 = args.index('-pf')
-            add_file_and_commit(args, index1)
-            push()
+            squash.add_file_and_commit(args, index1)
+            squash.push()
         elif match2:
             index2 = args.index('-fp')
-            add_file_and_commit(args, index2)
-            push()
+            squash.add_file_and_commit(args, index2)
+            squash.push()
 
-    # Add all files and commit/commit and push
+    # Add all files and commit/commit and squash.push
 
     # When arguments begin with -a only(add and commit only)
     if args[1] == '-a':
         index = args.index('-a')
-        add_all_and_commit(args, index)
+        squash.add_all_and_commit(args, index)
 
-    # When arguments begin with -a -p(add, commit and push them)
+    # When arguments begin with -a -p(add, commit and squash.push them)
     elif args[1] == '-a' and args[2] == '-p':
         index = args.index('-p')
-        add_all_and_commit(args, index)
-        push()
+        squash.add_all_and_commit(args, index)
+        squash.push()
 
-    # When arguments begin with -ap or -pa(add, commit and push files)
+    # When arguments begin with -ap or -pa(add, commit and squash.push files)
     elif (re.search('-ap', arg) for arg in args) or (re.search('-pa', arg) for arg in args):
         for arg in args:
             match1 = re.findall('-ap', arg)
@@ -66,16 +66,16 @@ def main():
         if match1 is not None:
             try:
                 index1 = args.index('-ap')
-                add_all_and_commit(args, index1)
-                push()
+                squash.add_all_and_commit(args, index1)
+                squash.push()
             except ValueError:
                 pass
 
         if match2 is not None:
             try:
                 index2 = args.index('-pa')
-                add_all_and_commit(args, index2)
-                push()
+                squash.add_all_and_commit(args, index2)
+                squash.push()
             except ValueError:
                 pass
 
